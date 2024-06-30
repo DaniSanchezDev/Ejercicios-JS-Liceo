3c87a170a69b5bb8fdd19a0981f24f18038f6b69
imdaano <danisanchezpsc@gmail.com>
2024-06-30 12:09:16 +0200

    add new JS  Code

--- 1-Guess-Number/scripts/index.js
+++ 1-Guess-Number/scripts/index.js
@@ -1,1 +1,74 @@
 const message = document.getElementById('message');
+const numberForm = document.getElementById('numberForm');
+const userNumberField = document.getElementById('userNumberField');
+const submitButton = document.getElementById('submitButton');
+const newNumberButton = document.getElementById('newNumberButton');
+const attemptList = document.getElementById('attemptList');
+const optionButton = document.getElementById('optionButton');
+let maxAttempts = 3;
+let currentAttempts = 0;
+let min = 1;
+let max = 10;
+
+const generateRandomNumber = (min = 1, max = 10) => {
+  Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let randomNumber = generateRandomNumber(min, max);
+console.log(randomNumber);
+
+const endGame = () => {
+  userNumberField.value = '';
+  submitButton.disabled = true;
+  newNumberButton.style.display = 'initial';
+};
+
+const checkNumber = number => {
+  if (number === randomNumber) {
+    message.innerHTML = `Random number guessed correctly in ${currentAttempts} attempt(s)!`;
+    endGame();
+  } else if (currentAttempts >= maxAttempts) {
+    message.innerHTML = `Random number was ${randomNumber}`;
+    endGame();
+  } else if (number < randomNumber) {
+    message.innerHTML = 'Random number is higher';
+  } else if (number > randomNumber) {
+    message.innerHTML = 'Random number is lower';
+  } 
+};
+
+const submitNumber = event => {
+  event.preventDefault();
+  const userNumber = +userNumberField.value;
+  currentAttempts++;
+  attemptList.innerHTML += `<li>${userNumber}</li>`;
+  checkNumber(userNumber);
+};
+numberForm.onsubmit = submitNumber;
+
+const generateNewNumber = () => {
+  message.innerHTML = '';
+  attemptList.innerHTML = '';
+  currentAttempts = 0;
+  randomNumber = generateRandomNumber(min, max);
+  console.log(randomNumber);
+  submitButton.disabled = false;
+  newNumberButton.style.display = 'none';
+};
+newNumberButton.onclick = generateNewNumber;
+
+const selectOption = () => {
+  const radioBtnEasy = document.getElementById('easy');
+  const radioBtnMedium = document.getElementById('medium');
+  if (radioBtnEasy.checked) {
+    min = 1;
+    max = 10;
+    maxAttempts = 3;
+  } else if (radioBtnMedium.checked) {
+    min = 1;
+    max = 50;
+    maxAttempts = 5;
+  }
+  generateNewNumber();
+};
+optionButton.onclick = selectOption;
