fad645e53a02e09bb5c064c081417725ae06df70
imdaano <danisanchezpsc@gmail.com>
2024-07-26 09:59:31 +0200

    add new js code

--- 1-TicTacToe/js/index.js
+++ 1-TicTacToe/js/index.js
@@ -1,3 +1,21 @@
 const messageText = document.getElementById('message');
 const newGameButton = document.getElementById('newGame');
-const images = document.getElementsByClassName('cell')
+const images = document.getElementsByClassName('cell');
+
+// Creamos variables para los turnos
+const PLAYER_1 = 1;
+const PLAYER_2 = 2;
+const EMPTY = 0;
+const DRAW = 0;
+
+let turn = PLAYER_1;
+// Ponemos el ganador al principio como vacio / nulo ( en java usaríamos -1)
+let winner = null;
+
+
+
+
+
+
+
+
