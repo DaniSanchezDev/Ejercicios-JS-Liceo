2ec7376ab5c69632aca4a0b99d679c0a861b7f14
imdaano <danisanchezpsc@gmail.com>
2024-07-30 10:07:20 +0200

    add new js code

--- 1-TicTacToe/js/index.js
+++ 1-TicTacToe/js/index.js
@@ -11,11 +11,20 @@ const DRAW = 0;
 let turn = PLAYER_1;
 // Ponemos el ganador al principio como vacio / nulo ( en java usaríamos -1)
 let winner = null;
+let board = [
+    [EMPTY,EMPTY,EMPTY,],
+    [EMPTY,EMPTY,EMPTY,],
+    [EMPTY,EMPTY,EMPTY,],
+];
+const PLAYER_1_IMAGE = './../img/x.jpg'
+const PLAYER_2_IMAGE = './../img/circle.png'
+const EMPTY_IMAGE = './../img/blank.png'
+// creamos funcion para generar numero random
+const generateRandomNumer = (min,max) => Math.floor(Math.random() + (max - min +1 )) + min;
 
+turn = generateRandomNumer(1,2);
 
-
-
-
+messageText.innerText = turn === PLAYER_1 ? 'Player 1 (X)' : 'Player 2 (0)';
 
 
 
